2442208948c200ee071bb89400bb850140719152
Ngoc Bui <ngocjr7@gmail.com>
2020-08-18 10:26:31 +0700

    c tokenizer

--- sctokenizer/c_tokenizer.py
+++ sctokenizer/c_tokenizer.py
@@ -1,8 +1,8 @@
 from __future__ import absolute_import
 
-from sctokenizer.tokenizer import Tokenizer
+from sctokenizer.cpp_tokenizer import CppTokenizer
 from sctokenizer.keyword.c_keyword import keyword_set
 from sctokenizer.token import TokenType, Token
 
-class CTokenizer(Tokenizer):
+class CTokenizer(CppTokenizer):
     pass
