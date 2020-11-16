d4a714a25499c68ad437d3825f2d464800322bf4
Ngoc Bui <ngocjr7@gmail.com>
2020-11-16 20:07:15 +0700

    update source

--- setup.py
+++ setup.py
@@ -3,12 +3,19 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+PROJECT_URLS = {
+    'Bug Tracker': 'https://github.com/ngocjr7/sctokenizer/issues',
+    'Documentation': 'https://github.com/ngocjr7/sctokenizer/blob/master/README.md',
+    'Source Code': 'https://github.com/ngocjr7/sctokenizer'
+}
+
 setup(name='sctokenizer',
       description='A Source Code Tokenizer',
       author='Ngoc Bui',
       long_description=long_description,
       long_description_content_type="text/markdown",
+      project_urls=PROJECT_URLS,
       author_email='ngocjr7@gmail.com',
-      version='0.0.2', 
+      version='0.0.5', 
       packages=find_packages(),
       python_requires='>=3.6')
